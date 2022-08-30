5f386156d4b21af41b2d8a1fddffa61c8e6d8d01
ArgeeNabas <argee.nabas@gmail.com>
2022-08-30 23:04:03 +1000

    added Header and Nav components

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hulu Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <Nav />
+      
+    </div>
+  )
+}
